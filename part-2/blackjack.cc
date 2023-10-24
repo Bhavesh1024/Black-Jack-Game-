f43157550004ad36d030586a99d69f3d41be6c86
Bhavesh1024 <143297804+Bhavesh1024@users.noreply.github.com>
2023-10-24 06:42:54 +0000

    Fixed Format to comply with google C++ format

--- part-2/blackjack.cc
+++ part-2/blackjack.cc
@@ -1,7 +1,7 @@
 // Julie Araujo
 // julie0211@csu.fullerton.edu
-// Julie0211
-// partners: Bhavesh
+// @Julie0211
+// Partners: @bhavesh1024
 
 #include <iostream>
 #include <string>
@@ -11,21 +11,22 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments(argv, argv + argc);
-  if (arguments.size() !=3) {
-    std::cout<< "error: you must supply two
-    arguments \n";
+  if (arguments.size() != 3) {
+    std::cout << "error: you must supply two arguments \n ";
     return 1;
   }
 
   std::string card_1 = arguments[1];
   std::string card_2 = arguments[2];
-  if (!IsCardName(card_1)||!IsCardName(card_2)) {
-    std::cout<< "error: unkown card \n";
+  if (!IsCardName(card_1) || !IsCardName(card_2)) {
+    std::cout << "error: unkown card \n";
     return 1;
   }
 
-  int total_score = TwoCardHandScore (card_1, card_2); {
-  std::cout<< total_score << std::end1;
+  int total_score = TwoCardHandScore(card_1, card_2);
+  {
+    std::cout << total_score << std::endl;
 
-  return 0;
+    return 0;
+  }
 }
